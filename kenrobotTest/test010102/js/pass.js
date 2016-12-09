9278c35aa68f473bb1170893087c67fe1db771d2
unknown <>
2016-12-09 19:13:07 +0800

    for bo ge

--- kenrobotTest/test010102/js/pass.js
+++ kenrobotTest/test010102/js/pass.js
@@ -2,6 +2,7 @@ function passtest() {
 	if (allquesion[1] == 1 && allquesion[0] == 1) {
 		var passpage = document.getElementById("passPage"); 
 		passpage.style.display="block";
+		$("#passPage").animate({opacity: 1}, 1000);
 	}
 	else{
 		console.log("Dont Finished");
--- kenrobotTest/test010106-/js/pass.js
+++
@@ -1,9 +0,0 @@
-function passtest() {
-	if (allquesion[1] == 1 && allquesion[0] == 1) {
-		var passpage = document.getElementById("passPage"); 
-		passpage.style.display="block";
-	}
-	else{
-		console.log("Dont Finished");
-	}
-}
